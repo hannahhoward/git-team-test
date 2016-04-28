baa8429f0eb49aa82986d877034b9194791326f4
Mike Wong <mikewong79@gmail.com>
2016-04-28 09:29:05 -0700

    add mike route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/mike', function(req, res, next) {
+  res.render('mike', { title: 'Mike' });
+});
+
 module.exports = router;
